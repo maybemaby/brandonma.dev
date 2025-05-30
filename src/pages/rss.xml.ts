ffe7210aceff7be44e60eb4f2d486450e0a6174e
maybemaby <74226064+maybemaby@users.noreply.github.com>
2025-05-29 23:55:40 -0700

    add rss feed

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,22 @@
+import rss from "@astrojs/rss";
+import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
+
+export const prerender = false;
+
+export const GET: APIRoute = async (context) => {
+  const blog = await getCollection("blog", (e) => !e.data.draft);
+  return rss({
+    title: "Blog - Brandon Ma",
+    description:
+      "Blog about engineering, software, and other written thoughts.",
+    site: "https://brandonma.dev",
+    items: blog.map((post) => ({
+      title: post.data.title,
+      description: post.data.metaDescription,
+      author: "Brandon Ma",
+      pubDate: post.data.publishedAt,
+      link: `/blog/${post.slug}`,
+    })),
+  });
+};
